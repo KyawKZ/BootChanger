0150dc9541bc3946743400f53cdbc4d85eb9358e
kyawk <kyawk@DESKTOP-JI8827H>
2021-12-03 05:20:08 +0630

    Add project files.

---
+++ BootChanger/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace BootChanger
+{
+    public partial class Form1 : Form
+    {
+        string s,g;
+        public Form1()
+        {
+            InitializeComponent();
+        }                      
+        ///
+        ///Mount EFI partition
+        ///
+        private void ESPMount()
+        {
+            richTextBox1.AppendText("Mounting EFI System Partition : ", Color.Yellow);
+            try
+            {
+                ProcessStartInfo esp = new ProcessStartInfo()
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/c mountvol Z: /S",
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                };
+                Process.Start(esp).WaitForExit();
+                richTextBox1.AppendText("Done!", Color.LimeGreen);
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.AppendText("Error!", Color.Red);
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        ///
+        /// Unmount EFI System Partition
+        /// 
+        private void ESPUnmount()
+        {
+            ProcessStartInfo esp_u = new ProcessStartInfo()
+            {
+                FileName = "cmd.exe",
+                Arguments = "/c mountvol Z: /D",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+            Process.Start(esp_u).WaitForExit();
+        }
+        ///
+        /// Execute bcdedit command
+        ///
+        private void Set_bootloader()
+        {
+            var p = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = g,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                }
+            };
+            p.Start();
+            p.WaitForExit();
+            string o= p.StandardOutput.ReadToEnd();
+            richTextBox1.AppendText(Environment.NewLine+o,Color.LimeGreen);
+        }
+        ///
+        /// List Current Bootloader folder
+        ///
+        public void List_Bootloader()
+        {
+            richTextBox1.AppendText("Scanning Bootloaders : ", Color.Yellow);
+            if (Directory.Exists(@"Z:\EFI\Microsoft"))
+            {
+                if (File.Exists(@"Z:\EFI\Microsoft\Boot\bootmgfw.efi"))
+                {
+                    richTextBox1.AppendText(Environment.NewLine +"Found Windows Boot Manager", Color.LimeGreen);
+                    comboBox1.Items.Add("Windows Boot Manager");
+                }
+            }
+            if (Directory.Exists(@"Z:\EFI\CLOVER"))
+            {
+                if (File.Exists(@"Z:\EFI\CLOVER\CLOVERX64.efi"))
+                {
+                    richTextBox1.AppendText(Environment.NewLine +"Found Clover Bootloader", Color.LimeGreen);
+                    comboBox1.Items.Add("Clover Bootloader");
+                }
+            }
+            if (Directory.Exists(@"Z:\EFI\OC"))
+            {
+                if (File.Exists(@"Z:\EFI\OC\Opencore.efi"))
+                {
+                    richTextBox1.AppendText(Environment.NewLine+"Found Opencore Bootloader", Color.LimeGreen);
+                    comboBox1.Items.Add("Opencore Bootloader");
+                }
+            }
+            var gd = Directory.GetDirectories(@"Z:\EFI");
+            foreach(string dir in gd)
+            {
+                if(dir!="Boot" && dir!="CLOVER" && dir!="Opencore" && dir != "Microsoft")
+                {
+                    if (File.Exists(dir + "\\grubx64.efi"))
+                    {
+                        richTextBox1.AppendText(Environment.NewLine+"Found Grub Bootloader", Color.LimeGreen);
+                        comboBox1.Items.Add("Grub Bootloader");
+                        s = Path.GetFullPath(dir + "\\grubx64.efi");                        
+                    }
+                }
+            }
+            comboBox1.SelectedIndex = 0;
+         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Current BootLoader List";
+            richTextBox1.ReadOnly = true;
+            ESPMount();
+            List_Bootloader();
+        }
+
+        private void AppClose(object sender, FormClosingEventArgs e)
+        {
+            ESPUnmount();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.facebook.com/hackintoshmm");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText(Environment.NewLine+"Setting Default Bootloader : ", Color.Yellow);
+            if (comboBox1.SelectedItem.ToString().Contains("CLOVER"))
+            {
+                g = "/c bcdedit /set {bootmgr} path EFI\\CLOVER\\CLOVERX64.efi";
+            }
+            else if (comboBox1.SelectedItem.ToString().Contains("Opencore"))
+            {
+                g = "/c bcdedit /set {bootmgr} path EFI\\Opencore\\Opencore.efi";
+            }
+            else if (comboBox1.SelectedItem.ToString().Contains("Windows"))
+            {
+                g = "/c bcdedit /set {bootmgr} path EFI\\Microsoft\\Boot\\bootmgfw.efi";
+            }
+            else if (comboBox1.SelectedItem.ToString().Contains("Grub"))
+            {
+                string pt=s.Replace("Z:\\","");
+                g="/c bcdedit /set {bootmgr} path "+pt;
+            }
+            else
+            {
+                richTextBox1.AppendText("Select a Bootloader",Color.Red);               
+            }
+            Set_bootloader();
+        }
+    }
+    public static class RichTextBoxExtension
+    {
+        public static void AppendText(this RichTextBox box, string text, Color color)
+        {
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+            box.SelectionColor = color;
+            box.AppendText(text);
+            box.SelectionColor = box.ForeColor;
+        }
+    }
+}
---
+++ BootChanger/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BootChanger")]
+[assembly: AssemblyDescription("Set Default bootloader for dualboot systems")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JJLab")]
+[assembly: AssemblyProduct("BootChanger")]
+[assembly: AssemblyCopyright("Copyright@JJLab")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("62eb5b48-2ba9-409c-903d-ca6f2b1fc2f2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
